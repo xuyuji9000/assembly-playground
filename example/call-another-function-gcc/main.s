32f997f2f462a76f72411c4a6b88ce720258b7dc
Karl Xu <karl.yogi.xu@gmail.com>
2021-03-11 12:24:17 +0800

    add assembly code

---
+++ example/call-another-function-gcc/main.s
@@ -0,0 +1,49 @@
+	.file	"main.c"
+	.intel_syntax noprefix
+	.text
+	.globl	func
+	.type	func, @function
+func:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	DWORD PTR [rbp-20], edi
+	mov	DWORD PTR [rbp-4], 1
+	mov	eax, DWORD PTR [rbp-20]
+	mov	DWORD PTR [rbp-8], eax
+	mov	eax, DWORD PTR [rbp-8]
+	add	DWORD PTR [rbp-4], eax
+	mov	eax, DWORD PTR [rbp-4]
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	func, .-func
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 16
+	mov	edi, 10
+	call	func
+	mov	DWORD PTR [rbp-4], eax
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 8.3.1 20191121 (Red Hat 8.3.1-5)"
+	.section	.note.GNU-stack,"",@progbits
